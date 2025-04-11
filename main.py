f268f75c45d98342edbac8fe2582942ca2f78d10
12lase <123396109+12lase@users.noreply.github.com>
2025-04-10 19:49:47 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,15 +4,18 @@ from queue import Queue
 from producer import start_producer
 from consumer import start_consumer
 
+#load url from input file - returns a list of urls stripped of new lines and spaces
 def load_urls(filename):
     with open(filename, "r") as f:
         return [line.strip() for line in f.readlines() if line.strip()]
 
 def main():
+    #takes one CLA - the output file for extracted urls. def:output.txt
     output_file = sys.argv[1] if len(sys.argv) > 1 else "output.txt"
     link_queue = Queue()
     urls = load_urls("urls.txt")
 
+    #producer & consumer threads
     producer_thread = threading.Thread(target=start_producer, args=(urls, link_queue))
     consumer_thread = threading.Thread(target=start_consumer, args=(link_queue, output_file))
 
