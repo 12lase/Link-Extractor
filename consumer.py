06e58d4864641e9b05303da2676e03f161172844
12lase <123396109+12lase@users.noreply.github.com>
2025-04-10 19:50:34 -0500

    Update consumer.py

--- consumer.py
+++ consumer.py
@@ -6,24 +6,31 @@ import threading
 # Shared lock so multiple consumers don’t write to the file at the same time
 write_lock = threading.Lock()
 
+# BeautifulSoup parses all the html using built in python parser
+# returns list of formated links for url passed 
 def extract_links(html, base_url):
     soup = BeautifulSoup(html, "html.parser")
     return [urljoin(base_url, a['href']) for a in soup.find_all("a", href=True)]
 
+# Extracts links from queue and outputs them to an output file
 def start_consumer(queue, output_file="output.txt"):
     with open(output_file, "w", encoding="utf-8") as f:
         while True:
-            item = queue.get()
+            item = queue.get() # Wait for an item from the queue
 
             if item is None:
+                # Signal from producer that all work is done
                 print("[Consumer] No more items. Exiting.")
                 break
 
-            url, html = item
+            url, html = item # Unpack the URL and HTML string
             print(f"[Consumer] Extracting from: {url}")
 
             try:
+                # Parse HTML and extract absolute links
                 links = extract_links(html, url)
+
+                # Ensure only one thread writes to the file at a time
                 with write_lock:
                     f.write(f"# Links from: {url}\n")
                     for link in links:
